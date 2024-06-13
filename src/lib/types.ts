6ce4fea4dbef94c48a6fdc2534ee7e1a134c5361
Maicon Filippsen <filippsen@users.noreply.github.com>
2024-06-13 12:38:48 -0300

    Bump version to 0.3.1

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,5 @@
 export const AppName = "OpenOdin DataWallet (official)";
-export const AppVersion = "0.3.0";
+export const AppVersion = "0.3.1";
 
 /**
  * Cryptographic keys are stored as integers so they can be seamlessly serialized to JSON.
